ba0be3c8b9228271cee3fb15786e172861af7651
johndevera7e7 <john.devera.7e7@itb.cat>
2025-11-18 10:14:16 +0100

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        const string AnyKeyContinue = "Press any key to continue";
+        const string MenuTitle = "===== MAIN MENU - CODEQUEST =====";
+        const string WelcomeMessage = "== Welcome, {0} the {1} with level {2} ==";
+        const string MenuOption1 = "1. Train your wizard";
+        const string MenuOption2 = "2. Check the dungeon";
+        const string MenuOption3 = "3. Loot the mine";
+        const string MenuOption4 = "4. Show inventory";
+        const string MenuOption5 = "5. Buy items";
+        const string MenuOption6 = "6. Show attacks by LVL";
+        const string MenuOption7 = "7. Decode ancient scrolls";
+        const string MenuOptionExit = "0. Exit game";
+        const string MenuPrompt = "Choose an option (1-3) - (0) to exit: ";
+        const string InputErrorMessage = "Invalid input. Please enter a number between 0 and 3.";
+
+        int op = 0;
+        string wizardName;
+
+        Random rnd = new Random();
+
+        Console.WriteLine("What's your name, oh destined one?");
+        wizardName = Console.ReadLine();
+
+        do
+        {
+            Console.WriteLine(MenuTitle);
+            Console.WriteLine(WelcomeMessage);
+            Console.WriteLine(MenuOption1);
+            Console.WriteLine(MenuOption2);
+            Console.WriteLine(MenuOption3);
+            Console.WriteLine(MenuOption4);
+            Console.WriteLine(MenuOption5);
+            Console.WriteLine(MenuOption6);
+            Console.WriteLine(MenuOption7);
+            Console.WriteLine(MenuOptionExit);
+            Console.Write(MenuPrompt);
+
+            try
+            {
+                op = Convert.ToInt32(Console.ReadLine());
+                switch (op) {
+                    case 1:
+
+                        break;
+                        
+                } 
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(InputErrorMessage);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(InputErrorMessage);
+            }
+
+        } while (op != 0);
+    }
+}
